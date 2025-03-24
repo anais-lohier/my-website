092e8f31979a79f5a477145781737c576f94efa6
anais-lohier <alohier21@gmail.com>
2025-03-23 21:24:36 -0600

    need to edit map points

--- script.js
+++ script.js
@@ -1,4 +1,27 @@
-function showMessage() {
-    document.getElementById("message").innerText = "Hello! You clicked the button!";
-}
-
+document.addEventListener("DOMContentLoaded", () => {
+    const sasMap = document.getElementById("sas-map");
+  
+    if (sasMap) {
+      sasMap.addEventListener("mouseover", () => {
+        sasMap.innerHTML = `
+          <ul>
+            <li>Japan 🇯🇵</li>
+            <li>Vietnam 🇻🇳</li>
+            <li>India 🇮🇳</li>
+            <li>Kenya 🇰🇪</li>
+            <li>South Africa 🇿🇦</li>
+            <li>Brazil 🇧🇷</li>
+            <li>Morocco 🇲🇦</li>
+          </ul>`;
+      });
+  
+      sasMap.addEventListener("mouseout", () => {
+        sasMap.innerHTML = "Hover to see destinations ✈️";
+      });
+    }
+  });
+  
+  function toggleEssay() {
+    const essay = document.getElementById('essay-container');
+    essay.style.display = essay.style.display === 'none' ? 'block' : 'none';
+  }
